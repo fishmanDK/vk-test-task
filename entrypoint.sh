7bf61d5baa68f5db2b1759a7784a51729542088b
Denis Svechnikov <denis.23weer@gmail.com>
2024-03-19 01:03:27 +0300

    add migrations docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,28 @@
+#set -e
+#
+#until PGPASSWORD=$POSTGRES_PASSWORD psql -h "localhost" -U "$POSTGRES_USER" -c '\q'; do
+# echo "Postgres is unavailable - sleeping"
+# sleep 1
+#done
+#
+#echo "Postgres is up - executing migrations"
+#psql -h "localhost" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/000001_init.up.sql
+#
+#exec postgres
+
+#!/bin/sh
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "localhost" -U "$POSTGRES_USER" -c '\q'; do
+# Ожидание доступности PostgreSQL
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -c '\q'; do
  echo "Postgres is unavailable - sleeping"
  sleep 1
 done
 
 echo "Postgres is up - executing migrations"
-psql -h "localhost" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/000001_init.up.sql
 
+# Выполнение миграций
+psql -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /schema/000001_init.up.sql
+
+# Запуск основного процесса (в данном случае, postgres)
 exec postgres
